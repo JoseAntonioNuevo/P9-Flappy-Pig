2936fc0a2718bf658dbaf0e7b625ef7940226596
Emma Garcia <egarciasanti@uoc.edu>
2020-11-28 12:53:08 +0100

    Colision y trigger Roca

--- P9Game/Assets/Scripts/IARoca.cs
+++ P9Game/Assets/Scripts/IARoca.cs
@@ -24,4 +24,12 @@ public class IARoca : MonoBehaviour
     {
         transform.position += new Vector3(speed * Time.deltaTime, 0);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
 }
